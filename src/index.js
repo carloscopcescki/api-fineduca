5eeeda96bdf9cc68933f5ae02f496a45142873ee
Copcescki <carlos.copcescki@gmail.com>
2025-10-05 00:41:54 -0300

    fix: conexão com vercel

--- src/index.js
+++ src/index.js
@@ -13,13 +13,16 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 connectDatabase();
+
+app.use(cors({
+  origin: ["https://fin-educa.vercel.app", "http://localhost:5500"],
+  methods: ["GET", "POST", "PATCH"],
+  credentials: true
+}));
+
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/auth", authRoute);
 app.use("/doc", swaggerRoute);
-
-app.use(cors({
-  origin: "https://fin-educa.vercel.app",
-}));
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
